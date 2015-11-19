b66cce75bf1b45fbecd3a675ab827c3cb2a9f644
admin <nuwan.thusitha@gtnexus.com>
2015-11-19 16:12:59 +0530

    Change home page to mimic prototype on AWS

--- wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
+++ wp-content/uploads/revisr-backups/revisr_wp_revisr.sql
@@ -19,12 +19,12 @@ CREATE TABLE `wp_revisr` (
   `event` varchar(42) NOT NULL,
   `user` varchar(60) DEFAULT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `wp_revisr` WRITE;
 /*!40000 ALTER TABLE `wp_revisr` DISABLE KEYS */;
-INSERT INTO `wp_revisr` VALUES (1,'2015-11-19 10:23:08','Successfully created a new repository.','init','admin'),(2,'2015-11-19 10:24:25','Successfully backed up the database.','backup','admin'),(3,'2015-11-19 10:24:27','Committed <a href=\"http://172.30.14.102:8888/appx/wp-admin/admin.php?page=revisr_view_commit&commit=40ae9cf&success=true\">#40ae9cf</a> to the local repository.','commit','admin');
+INSERT INTO `wp_revisr` VALUES (1,'2015-11-19 10:23:08','Successfully created a new repository.','init','admin'),(2,'2015-11-19 10:24:25','Successfully backed up the database.','backup','admin'),(3,'2015-11-19 10:24:27','Committed <a href=\"http://172.30.14.102:8888/appx/wp-admin/admin.php?page=revisr_view_commit&commit=40ae9cf&success=true\">#40ae9cf</a> to the local repository.','commit','admin'),(4,'2015-11-19 10:24:43','Successfully backed up the database.','backup','admin');
 /*!40000 ALTER TABLE `wp_revisr` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
